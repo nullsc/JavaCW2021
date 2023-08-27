19c5b529332b962a2f791a55341942f39c10c700
nullsc <53406164+nullsc@users.noreply.github.com>
2023-08-27 22:37:29 +0100

    Create HasPages.java

---
+++ CW2/HasPages.java
@@ -0,0 +1,15 @@
+/**
+ * Interface for anything that has a number of pages:
+ * printed books, journals, ...
+ *
+ * @author 
+ */
+public interface HasPages {
+
+    /**
+     * Returns the number of pages.
+     *
+     * @return the number of pages
+     */
+    int getPages();
+}
